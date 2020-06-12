e57a579946cdfd824b5aaebad08e737e2f85880f
G-JD1-2019-10-01_Budzko <bamaikoxygeno22@gmail.com>
2020-06-12 20:50:46 +0300

    create second java

---
+++ src/lesson/second.java
@@ -0,0 +1,16 @@
+package lesson;
+
+public class second {
+
+    public static void main(String[] args) {
+
+        int number1 = 17;
+        int number2 = 21;
+
+        int number3 = (number1 + number2) * 3;
+        System.out.println(number3);
+
+
+
+    }
+}
